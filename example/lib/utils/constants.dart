dc7b499eaaa12ae78951a3fb90093cd53dd61699
atharv-bhadange <bhadange.atharv@gmail.com>
2023-03-09 15:40:29 +0530

    iOS connecting function completed

--- example/lib/utils/constants.dart
+++ example/lib/utils/constants.dart
@@ -1,4 +1,7 @@
-
 //to set the duration for scannning unprovisioned devices
-const Duration kconstDuration=Duration(seconds: 5);
-  
+const Duration kconstDuration = Duration(seconds: 10);
+const int companyId = 0x05C3;
+
+const int controlGetOpCode = 0x0001;
+const int controlSetOpCode = 0x0002;
+const int dataGetOpCode = 0x0000;
