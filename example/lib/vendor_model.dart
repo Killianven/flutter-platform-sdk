4dc0fe7b18b6534c2f6d15525c5c26028a6d9e5d
Sharvi Endait <sharviendait@gmail.com>
2023-04-10 17:48:59 +0530

    fixes made

--- example/lib/vendor_model.dart
+++ example/lib/vendor_model.dart
@@ -1,11 +1,12 @@
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:golain_example/vendor_model_rgb.dart';
 
 import 'protobuf/rgb_on_off/rgb_on_off.pb.dart';
 import 'scanbloc/scan_bloc.dart';
-import 'vendor_model_rgb.dart';
 
 class VendorModel extends StatelessWidget {
   final DiscoveredDevice device;
@@ -32,183 +33,144 @@ class VendorModel extends StatelessWidget {
               style: TextStyle(
                 fontSize: 20,
               ),
-            );
-          }
-          if (state is VendorModelDataFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Vendor Model Message: ${state.error}'),
-              ),
-            );
-          }
-          if (state is VendorModelControlGetSuccess) {
-            final message = shadow.fromBuffer(state.data.message.toList());
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Vendor Model Message: $message'),
-              ),
-            );
-          }
-          if (state is VendorModelControlFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Vendor Model Message: ${state.error}'),
-              ),
-            );
-          }
+            ),
+          ),
+          BlocConsumer<ScanBloc, ScanState>(listener: (context, state) {
+            if (state is VendorModelDataFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Vendor Model Message: ${state.error}'),
+                ),
+              );
+            }
+            if (state is VendorModelControlGetSuccess) {
+              final message = shadow.fromBuffer(state.data.message.toList());
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Vendor Model Message: $message'),
+                ),
+              );
+            }
+            if (state is VendorModelControlFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Vendor Model Message: ${state.error}'),
+                ),
+              );
+            }
 
-          if (state is DeprovisioningSuccess) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.message),
-              ),
-            );
-          }
+            if (state is DeprovisioningSuccess) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message),
+                ),
+              );
+            }
 
-          if (state is DeprovisioningFailure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.message),
-              ),
-            );
-          }
-        },
-        builder: (context, state) {
-          return ListView(
-            children: [
-              ExpansionTile(
-                title: const Text('Vendor Model Data plane '),
-                children: [
-                  TextField(
-                    decoration:
-                        const InputDecoration(hintText: 'Element Address'),
-                    controller: dataPlaneElementAddressController,
-                  ),
-                );
-              }
-              if (state is VendorModelDataFailure) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Vendor Model Message: ${state.error}'),
-                  ),
-                );
-              }
-              if (state is VendorModelControlGetSuccess) {
-                final message = shadow.fromBuffer(state.data.message.toList());
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Vendor Model Message: $message'),
-                  ),
-                );
-              }
-              if (state is VendorModelControlFailure) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Vendor Model Message: ${state.error}'),
-                  ),
-                );
-              }
-            },
-            builder: (context, state) {
-              return Expanded(
-                child: SizedBox(
-                  height: 200,
-                  child: ListView(
+            if (state is DeprovisioningFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message),
+                ),
+              );
+            }
+          }, builder: (context, state) {
+            return Expanded(
+              child: SizedBox(
+                height: 200,
+                child: ListView(children: [
+                  ExpansionTile(
+                    title: const Text('Vendor Model Data plane '),
                     children: [
-                      ExpansionTile(
-                        title: const Text('Vendor Model Data plane '),
-                        children: [
-                          TextField(
-                            decoration: const InputDecoration(
-                                hintText: 'Element Address'),
-                            controller: dataPlaneElementAddressController,
-                          ),
-                          TextField(
-                            decoration: const InputDecoration(
-                                hintText: 'Data Request Type'),
-                            controller: dataPlaneController,
-                          ),
-                          TextButton(
-                            onPressed: () async {
-                              BlocProvider.of<ScanBloc>(context).add(
-                                VendorModelDataGetRequested(
-                                  int.parse(
-                                      dataPlaneElementAddressController.text),
-                                  dataPlaneController.text,
-                                ),
-                              );
-                            },
-                            child: const Text('Get'),
-                          ),
-                        ],
+                      TextField(
+                        decoration:
+                            const InputDecoration(hintText: 'Element Address'),
+                        controller: dataPlaneElementAddressController,
                       ),
-                      ExpansionTile(
-                        title: const Text("Vendor Mode Control Plane"),
-                        children: [
-                          TextField(
-                            key: const ValueKey(
-                                'module-send-generic-level-address'),
-                            decoration: const InputDecoration(
-                                hintText: 'Element Address'),
-                            controller: controlPlaneElementAddressController,
-                          ),
-                          TextButton(
-                            onPressed: () async {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => RGBProtobuf(
-                                    selectedElementAddress: int.parse(
-                                        controlPlaneElementAddressController
-                                            .text),
-                                  ),
-                                ),
-                              );
-                            },
-                            child: const Text('Set'),
-                          ),
-                          TextButton(
-                            onPressed: () async {
-                              BlocProvider.of<ScanBloc>(context).add(
-                                VendorModelControlGetRequested(
-                                  int.parse(controlPlaneElementAddressController
-                                      .text),
-                                  Uint8List(0),
-                                ),
-                              );
-                            },
-                            child: const Text('Get'),
-                          ),
-                        ],
-                      )
+                      TextField(
+                        decoration: const InputDecoration(
+                            hintText: 'Data Request Type'),
+                        controller: dataPlaneController,
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          BlocProvider.of<ScanBloc>(context).add(
+                            VendorModelDataGetRequested(
+                              int.parse(dataPlaneElementAddressController.text),
+                              dataPlaneController.text,
+                            ),
+                          );
+                        },
+                        child: const Text('Get'),
+                      ),
                     ],
                   ),
-                ],
-              ),
-              ExpansionTile(
-                key: const ValueKey('module-send-deprovisioning-form'),
-                title: const Text('Send a deprovisioning'),
-                children: <Widget>[
-                  TextField(
-                    key: const ValueKey('module-send-deprovisioning-address'),
-                    decoration:
-                        const InputDecoration(hintText: 'Element Address'),
-                    controller: deprovisionElementAddressController,
+                  ExpansionTile(
+                    title: const Text("Vendor Mode Control Plane"),
+                    children: [
+                      TextField(
+                        key:
+                            const ValueKey('module-send-generic-level-address'),
+                        decoration:
+                            const InputDecoration(hintText: 'Element Address'),
+                        controller: controlPlaneElementAddressController,
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => RGBProtobuf(
+                                selectedElementAddress: int.parse(
+                                    controlPlaneElementAddressController.text),
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text('Set'),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          BlocProvider.of<ScanBloc>(context).add(
+                            VendorModelControlGetRequested(
+                              int.parse(
+                                  controlPlaneElementAddressController.text),
+                              Uint8List(0),
+                            ),
+                          );
+                        },
+                        child: const Text('Get'),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    onPressed: () {
-                      BlocProvider.of<ScanBloc>(context).add(
-                        DeprovisioningRequested(
-                          int.parse(deprovisionElementAddressController.text),
-                        ),
-                      );
-                    },
-                    child: const Text('Send node reset'),
+                  ExpansionTile(
+                    key: const ValueKey('module-send-deprovisioning-form'),
+                    title: const Text('Send a deprovisioning'),
+                    children: <Widget>[
+                      TextField(
+                        key: const ValueKey(
+                            'module-send-deprovisioning-address'),
+                        decoration:
+                            const InputDecoration(hintText: 'Element Address'),
+                        controller: deprovisionElementAddressController,
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          BlocProvider.of<ScanBloc>(context).add(
+                            DeprovisioningRequested(
+                              int.parse(
+                                  deprovisionElementAddressController.text),
+                            ),
+                          );
+                        },
+                        child: const Text('Send node reset'),
+                      )
+                    ],
                   )
-                ],
-              )
-            ],
-          );
-        },
+                ]),
+              ),
+            );
+          }),
+        ],
       ),
     );
   }
