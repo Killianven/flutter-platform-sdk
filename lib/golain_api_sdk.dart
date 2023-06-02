f693826a80c7014885b71ac924a49d199c039174
SharviE29 <sharviendait@gmail.com>
2023-06-02 21:07:31 +0530

    api changes

--- lib/golain_api_sdk.dart
+++ lib/golain_api_sdk.dart
@@ -3,21 +3,37 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:golain/models/devices.dart';
 import 'package:golain/models/fleet.dart';
+
+import 'models/auth.dart';
 // import 'package:golain/constants.dart';
 
 
  
- class GolainApi{
+ class GolainAPI{
 
   Dio dio = Dio();
+  String? baseUrl;
+  String? orgId;
+  String? projectId;
+  Auth?   auth;
+  String? fleetUrl;
+
+  GolainAPI (String baseUrl, String orgId, String projectId,String fleetUrl, Auth auth){
+    this.baseUrl = baseUrl;
+    this.orgId = orgId;
+    this.projectId = projectId;
+    this.auth = auth;
+    this.fleetUrl = fleetUrl;
+    dio.options.headers['Authorization'] = "Bearer ${auth.authToken}";
+    dio.options.headers['Content-Type'] = 'application/json';
+    dio.options.headers['ORG-ID'] = orgId;
+  }
 
  Future<Map<String, dynamic>> getDeviceShadow(
-      String fleetId, String deviceId, String authToken, String orgId, String projectId, String baseUrl) async {
+     Device device) async {
     try {
-      dio.options.headers['Authorization'] = "Bearer $authToken";
-      dio.options.headers['ORG-ID'] = orgId;
       Response response = await dio.get(
-        "$baseUrl/$projectId/fleets/$fleetId/devices/$deviceId/shadow",
+        "$baseUrl/${device.project_id}/fleets/${device.fleet_id}/devices/${device.id}/shadow",
       );
       if (response.statusCode == 200) {
         return response.data;
@@ -25,25 +41,18 @@ import 'package:golain/models/fleet.dart';
         throw Exception('Failed to get shadow');
       }
     } catch (e) {
-      //print(response.data);
+      
       throw Exception('Failed to get shadow response');
     }
   }
 
 //post request to create shadow
   Future<Map<String, dynamic>> createDeviceShadow(
-    String projectId,
-      String fleetId,
-      String deviceId,
-      String authToken,
-      String orgId,
-      String shadowData,
-      String baseUrl) async {
+      Device device,
+      String shadowData,) async {
     try {
-      dio.options.headers['Authorization'] = "Bearer $authToken";
-      dio.options.headers['ORG-ID'] = orgId;
       final response = await dio.post(
-          '$baseUrl/$projectId/fleets/$fleetId/devices/$deviceId/shadow',
+          '$baseUrl/${device.project_id}/fleets/${device.fleet_id}/devices/${device.id}/shadow',
           data: shadowData);
       if (response.statusCode == 200) {
         return response.data;
@@ -57,18 +66,11 @@ import 'package:golain/models/fleet.dart';
 
 //put request to update shadow
   Future<Map<String, dynamic>> updateDeviceShadow(
-    String projectId,
-      String fleetID,
-      String deviceId,
-      String shadowData,
-      String authToken,
-      String orgId,
-      String baseUrl) async {
+    Device device,
+      String shadowData) async {
     try {
-      dio.options.headers['Authorization'] = "Bearer $authToken";
-      dio.options.headers['ORG-ID'] = orgId;
       final response = await dio.patch(
-          '$baseUrl/$projectId/fleets/$fleetID/devices/$deviceId/shadow',
+          '$baseUrl/${device.project_id}/fleets/${device.fleet_id}/devices/${device.id}/shadow',
           data: shadowData);
       if (response.statusCode == 200) {
         return response.data;
@@ -81,34 +83,15 @@ import 'package:golain/models/fleet.dart';
  }
 
 
- Future<List<Device>> getDevices(
-    String accessToken, String organisationId, String fleetId, String fleetUrl) async {
+ Future<List<Device>> getDevices(Fleet fleet) async {
   try {
-    dio.options.headers['Authorization'] = "Bearer $accessToken";
-    dio.options.headers['ORG-ID'] = organisationId;
-    Response response = await dio.get('fleetUrl/$fleetId/devices');
+    Response response = await dio.get('fleetUrl/${fleet.id}/devices');
     if (response.statusCode == 200) {
       log(response.data.toString());
       List<dynamic> data = response.data['data']['devices'];
       List<Device> devices = [];
       for (var device in data) {
-        Device newDevice = Device(
-          deviceId: device['id'],
-          deviceName: device['device_name'],
-          certificateId: device['certificate_id'],
-          currentShadow: device['current_shadow'],
-          shadowDefinitionId: device['shadow_definition_id'],
-          statusFlags: device['status_flags'],
-          connected: device['connected'],
-          lastSeen: device['last_seen'],
-          topicSlug: device['topic_slug'],
-          fleetId: device['fleet_id'],
-          projectId: device['project_id'],
-          orgId: device['org_id'],
-          updatedBy: device['updated_by'],
-          createdAt: device['created_at'],
-          pendingOtaUpdateId: device['pending_ota_update_id'],
-        );
+        Device newDevice = Device.fromJson(device);
         devices.add(newDevice);
       }
       return devices;
@@ -120,29 +103,15 @@ import 'package:golain/models/fleet.dart';
   }
 }
 
-Future<List<Fleet>> getFleets(String accessToken, String organisationId, String fleetUrl) async {
+Future<List<Fleet>> getFleets() async {
   try {
-    dio.options.headers['Authorization'] = "Bearer $accessToken";
-    dio.options.headers['ORG-ID'] = organisationId;
-    Response response = await dio.get(fleetUrl);
+    Response response = await dio.get(fleetUrl!);
     if (response.statusCode == 200) {
       List<dynamic> data = response.data['data']['fleets'];
       log(data.toString());
       List<Fleet> fleets = [];
       for (var fleet in data) {
-        Fleet newFleet = Fleet(
-            fleetId: fleet['id'],
-            fleetName: fleet['fleet_name'],
-            fleetDesc: fleet['fleet_description'],
-            shadowDefinitionId: fleet['shadow_definition_id'],
-            provisioningPolicyId: fleet['provisioning_policy_id'],
-            loggingPolicyId: fleet['logging_policy_id'],
-            userAuthenticationPolicyId: fleet['user_authentication_policy_id'],
-            otaPolicyId: fleet['ota_policy_id'],
-            projectId: fleet['project_id'],
-            orgId: fleet['org_id'],
-            createdAt: fleet['created_at'] 
-        );
+        Fleet newFleet = Fleet.fromJson(fleet);
         fleets.add(newFleet);
       }
       return fleets;
